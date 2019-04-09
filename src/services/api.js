c4416ae4554bacfd50fd40da417cc1cd26829a29
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-09 16:57:50 -0300

    fix: remove constraints

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,26 @@
-const baseUrl = 'https://api.spotify.com'
-const searchEndpoint = '/v1/search'
-const accessToken = 'BQDwLR_NCAbeDHS_5ap85BSXppslELG2gFRu_2hZXcmweYrStidnU3-A7bjw7FR7Kvly7lKOq63qko_eIz12ql80LuGO65sFUL_PMcINoiwjRGdXdP23M3XjMRB34b0hDZL1KOl0nQh-5RPOQdfdqC7nFz4Zcz9TACyaVGOjLxK0AqQiiQ'
+const baseUrl = 'https://api.spotify.com';
+const searchEndpoint = '/v1/search';
+const accessToken = '';
+
+const CLIENT_ID = '4b814332dcc04fb1aa099e6b0f1653ac';
+//const CLIENT_SECRET = 'd639c931f1fb42c7afb19b737a9d2605';
+const REDIRECT_URI = 'http://localhost:3000/callback';
+const SCOPE = 'user-read-private%20user-read-email';
 
 function searchSpotifyAPI(query, type) {
 	return fetch(`${baseUrl}${searchEndpoint}?q=${query}&type=${type}&access_token=${accessToken}`)
 		.then(response => response.json())
 }
 
+function getToken() {
+	return fetch(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=token&state=123`)
+		.then(response => response.json())
+		//.then(data => console.log(data))
+}
+
 const API = {
-	search: searchSpotifyAPI
+	search: searchSpotifyAPI,
+	authenticate: getToken
 }
 
 export default API
