d2a9d67c556139275e7a0f89fa326cfc83ae8fb7
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-09 01:20:33 -0300

    add: redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
 
 ReactDOM.render(
