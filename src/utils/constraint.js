a35e5b6e719103e88d6285c1f816ec397cc87f25
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-09 16:57:10 -0300

    add: variables to consraints

--- src/utils/constraint.js
+++ src/utils/constraint.js
@@ -4,14 +4,20 @@ const isMobile = typeof window.orientation !== 'undefined';
 const styles = {
 	colors: {
 		bgColor: '#191414',
+		gray: '#888',
 		darkGray: '#b3b3b3',
+		darkerGray: '#555',
 		white: '#FFF',
-		theme: '#1DB954'
+		black: '#000',
+		themeLight: '#1ED760',
+		theme: '#1aa34a',
+		themeDark: '#089918'
+
 	},
 	fonts: {
-		small: '14px',
-		medium: '18px',
-		large: isMobile ? '18px' : '24px'
+		small: isMobile ? '12px' : '24px',
+		medium: isMobile ? '18px' : '42px',
+		large: isMobile ? '24px' : '64px'
 	}
 }
 
