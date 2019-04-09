11688aad0392e4cf4d584f3c216ee7cb8e72ee6b
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-09 02:37:54 -0300

    refactor: connect to redux

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { changeTab } from '../../store/ducks/tab';
 
 import Container from './styles'
 
 import Tab from './Tab';
 
-const Tabs = () => (
-	<Container>
-		<Tab name="Artists" selected />
-		<Tab name="Albums"/>
-		<Tab name="Tracks"/>
-	</Container>
-)
+class Tabs extends React.Component {
+	handleClick = type => this.props.changeTab(type)
 
-export default Tabs;
+	render() {
+		const { tab } = this.props;
+		return (
+			<Container>
+				<Tab name="Artists" onClick={this.handleClick} selected={tab === 'Artists'} />
+				<Tab name="Albums" onClick={this.handleClick} selected={tab === 'Albums'} />
+				<Tab name="Tracks" onClick={this.handleClick} selected={tab === 'Tracks'} />
+			</Container>
+		)
+	}
+}
+
+const mapStateToProps = ({ tab }) => ({
+	tab
+})
+
+export default connect(
+	mapStateToProps,
+	{
+		changeTab
+	}
+)(Tabs);
