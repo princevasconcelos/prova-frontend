eba4a4c93531c627e2875cce3052f497416c37a1
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-09 01:19:06 -0300

    add: fonts

--- src/utils/constraint.js
+++ src/utils/constraint.js
@@ -1,7 +1,17 @@
+
+const isMobile = typeof window.orientation !== 'undefined';
+
 const styles = {
 	colors: {
 		bgColor: '#191414',
+		darkGray: '#b3b3b3',
+		white: '#FFF',
 		theme: '#1DB954'
+	},
+	fonts: {
+		small: '14px',
+		medium: '18px',
+		large: isMobile ? '18px' : '24px'
 	}
 }
 
