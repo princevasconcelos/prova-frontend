f5d6718aa3f14b1373e99ced1f9d34ea51eff891
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-09 01:19:50 -0300

    add: Muli font to global style

--- src/styles.js
+++ src/styles.js
@@ -3,6 +3,8 @@ import styled, { createGlobalStyle } from 'styled-components';
 import styles from './utils/constraint';
 
 const GlobalStyle = createGlobalStyle`
+	@import url('https://fonts.googleapis.com/css?family=Muli:200,400,600,700');
+
 	* {
 		margin: 0;
 		padding: 0;
@@ -16,10 +18,8 @@ const GlobalStyle = createGlobalStyle`
   body {
 		background-image: linear-gradient(to right bottom, rgb(70, 77, 83), rgb(0, 0, 0)), linear-gradient(transparent, rgb(0, 0, 0) 70%);
 		background-repeat: no-repeat;
+		font-family: 'Muli';
 	}
 `
 
 export { GlobalStyle };
-
-
-//background-image: linear-gradient(to right bottom, rgb(70, 77, 83), rgb(0, 0, 0)), linear-gradient(transparent, rgb(0, 0, 0) 70%);
