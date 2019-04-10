a2c51a0cb3131b99d1082207babff87d7712c4a2
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-10 02:39:50 -0300

    refactor: remove variables

--- src/services/api.js
+++ src/services/api.js
@@ -1,26 +1,13 @@
 const baseUrl = 'https://api.spotify.com';
 const searchEndpoint = '/v1/search';
-const accessToken = '';
 
-const CLIENT_ID = '4b814332dcc04fb1aa099e6b0f1653ac';
-//const CLIENT_SECRET = 'd639c931f1fb42c7afb19b737a9d2605';
-const REDIRECT_URI = 'http://localhost:3000/callback';
-const SCOPE = 'user-read-private%20user-read-email';
-
-function searchSpotifyAPI(query, type) {
+function searchSpotifyAPI(query, type, accessToken) {
 	return fetch(`${baseUrl}${searchEndpoint}?q=${query}&type=${type}&access_token=${accessToken}`)
 		.then(response => response.json())
 }
 
-function getToken() {
-	return fetch(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=token&state=123`)
-		.then(response => response.json())
-		//.then(data => console.log(data))
-}
-
 const API = {
 	search: searchSpotifyAPI,
-	authenticate: getToken
 }
 
 export default API
