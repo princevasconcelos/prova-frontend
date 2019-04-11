d5daf4f1eed0dbe2133b214626fc25efa15e95b0
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-11 02:44:12 -0300

    add: Results

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { SigninMessage } from './styles'
+import Resultlist from './styles'
 
-const Results = ({ authentication: { isAuthenticated }, results }) => isAuthenticated
- ? <ul>
-	 {results.data.map(result => <li>{result.name}</li>)}
- </ul>
- : <SigninMessage>You need to Sign in first</SigninMessage>
+import Login from '../Login'
 
-const mapStateToProps = ({ authentication, results }) => ({
+import Album from './Album';
+import Artist from './Artist';
+import Track from './Track';
+
+import { favoriteHandler } from '../../store/ducks/favorites'
+
+const Results = ({
+	authentication: { isAuthenticated },
+	results,
+	favoriteHandler,
+	favorites,
+	tab
+}) => isAuthenticated
+ ? <Resultlist type={tab}>
+		{results.data.map(result => {
+			if (tab === 'Artists'&& result.type === 'artist') return <Artist key={result.id} result={result} favoriteHandler={favoriteHandler} favorites={favorites} />
+			if (tab === 'Albums' && result.type === 'album') return <Album result={result} key={result.id} favoriteHandler={favoriteHandler} favorites={favorites} />
+			if (tab === 'Tracks' && result.type === 'track') return <Track result={result} key={result.id} favoriteHandler={favoriteHandler} favorites={favorites} />
+		})}
+ 	</Resultlist>
+ : <Login isAuthenticated={isAuthenticated}/>
+
+const mapStateToProps = ({ authentication, results, favorites, tab }) => ({
 	authentication,
-	results
+	results,
+	favorites,
+	tab
 })
 
-export default connect(mapStateToProps)(Results);
+export default connect(mapStateToProps, {
+	favoriteHandler
+})(Results);
--- src/components/Results/styles.js
+++ src/components/Results/styles.js
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
 
-import styles from '../../utils/constraint'
-
-const SigninMessage = styled.h2`
-	color: ${styles.colors.white};
+const Resultlist = styled.div`
+	display: flex;
+	justify-content: center;
+	flex-basis: 100vh;
+	flex-wrap: wrap;
+	max-width: ${({ type }) => getSizeByType(type)};
 `
 
-export { SigninMessage }
+const getSizeByType = type => {
+	return {
+		Albums: '2000px',
+		Artists: '1500px',
+		Tracks: '600px'
+	}[type]
+}
+
+export default Resultlist
