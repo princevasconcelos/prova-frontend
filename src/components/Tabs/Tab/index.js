623ac324d234f0dff0948f58b277ca1dfd2ec036
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-12 14:32:07 -0300

    remove tab

--- src/components/Tabs/Tab/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import StyledTab from './styles';
-
-const Tab = ({ name, selected, onClick }) =>
-<StyledTab onClick={() => onClick(name)} selected={selected}>{name}</StyledTab>
-
-export default Tab;
