e3d7988d657e4f3db32ea798bdd0a56651bc87f7
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-12 14:30:50 -0300

    refactor: album

--- src/components/Results/Album/index.js
+++ src/components/Results/Album/index.js
@@ -4,22 +4,40 @@ import Result from '../shared';
 
 import { Heart, Image } from '../shared/styles';
 
-const Album = ({
-	result: {
-		id = '',
-		images = []
-	},
-	favoriteHandler = () => {},
-	favorites = []
-}) =>
-	<Result>
-		<Heart
-			onClick={() => favoriteHandler(id)}
-			isFavorite={favorites.includes(id)}
-		/>
+import Modal from '../../Modal'
 
-		{ images.length > 0 &&
-			<Image src={images[0].url} alt="Album Logo" /> }
-	</Result>
+class Album extends React.Component {
+	state = {
+		showModal: false
+	}
+
+	handleClick = () => this.setState({ showModal: !this.state.showModal })
+
+	render() {
+		const {
+			result: {
+				id = '',
+				images = []
+			},
+			favoriteHandler = () => {},
+			favorites = []
+		} = this.props;
+
+		return (
+			<>
+				<Result handleClick={this.handleClick}>
+					<Heart
+						onClick={() => favoriteHandler(id)}
+						isFavorite={favorites.includes(id)}
+					/>
+
+					{ images.length > 0 &&
+						<Image src={images[0].url} alt="Album Logo" /> }
+				</Result>
+				<Modal isOpen={this.state.showModal}>ola prince</Modal>
+			</>
+		)
+	}
+}
 
 export default Album;
