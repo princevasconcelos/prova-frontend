a47e9157bbf5251bbd7b0675648f3cc35cf0733e
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-12 14:32:16 -0300

    add types

---
+++ src/components/Types/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { changeType } from '../../store/ducks/type';
+
+import Tabs from './styles'
+
+import Type from './Type';
+
+const Types = ({
+	query, type, authentication: { token }, getData, changeType
+}) => {
+	async function handleTypeChange(newType) {
+		changeType(newType);
+		getData(query, newType, token);
+	}
+	return (
+		<Tabs>
+			{console.log('<Types />')}
+			<Type name="Artists"
+				onClick={handleTypeChange}
+				type="artist"
+				selected={type === 'artist'} />
+
+			<Type name="Albums"
+				onClick={handleTypeChange}
+				type="album"
+				selected={type === 'album'} />
+
+			<Type name="Tracks"
+				onClick={handleTypeChange}
+				type="track"
+				selected={type === 'track'} />
+		</Tabs>
+	)
+}
+
+const mapStateToProps = ({ query, type, authentication }) => ({
+	query,
+	type,
+	authentication
+})
+
+export default connect(
+	mapStateToProps,
+	{
+		changeType,
+	}
+)(Types);
---
+++ src/components/Types/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Tabs = styled.nav`
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+	width: 100%;
+	height: 50px;
+	margin-top: 24px;
+`
+
+export default Tabs;
