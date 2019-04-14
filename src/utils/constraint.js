e5ec1ea91fe216c1a9073d9a0dec886633795ea6
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-14 14:46:54 -0300

    utils

--- src/utils/constraint.js
+++ src/utils/constraint.js
@@ -5,26 +5,33 @@ const styles = {
 		gray: '#888',
 		darkGray: '#b3b3b3',
 		darkerGray: '#555',
+		darkestGray: '#222',
 		white: '#FFF',
 		black: '#000',
 		theme: '#1aa34a',
 		popularity: {
 			hot: '#b20000',
-			cool: '#e0d100',
-			regular: '#02c0ab',
-			underground: '#194bff'
-		}
+			cool: '#3784DD',
+			regular: '#5D2D74',
+			underground: '#111'
+		},
+		lighterBlue: '#aab8c2',
+		lightestBlue: '#e1e8ed',
+		darkBlue: '#14171a'
 	},
 	sizes: {
 		xsmall: '11px',
 		small: '14px',
 		medium: '18px',
 		large: '24px',
+		xlarge: '32px'
 	},
 	screens: {
 		tablet: '760px',
 		desktop: '1200px'
 	}
 }
+
+
 
 export default styles;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,33 @@
+
+import styles from './constraint'
+
 function millisToMinutesAndSeconds(millis) {
   var minutes = Math.floor(millis / 60000);
   var seconds = ((millis % 60000) / 1000).toFixed(0);
   return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
 
-export { millisToMinutesAndSeconds }
+function getPopularityByValue(value) {
+	if (value > 80) return {
+		name: 'Hot',
+		color: `${styles.colors.popularity.hot}`
+	};
+	if (value > 60) return {
+		name: 'Cool',
+		color: `${styles.colors.popularity.cool}`
+	};
+	if (value > 30) return {
+		name: 'Regular',
+		color: `${styles.colors.popularity.regular}`
+	};
+	if (value > 0) return {
+		name: 'Underground',
+		color: `${styles.colors.popularity.underground}`
+	};
+	return {
+		name: '',
+		color: 'transparent'
+	};
+}
+
+export { millisToMinutesAndSeconds, getPopularityByValue }
